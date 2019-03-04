7587fc1564f029417da45bfff8e2ac7e5886d0c7
Shmood00 <43577426+Shmood00@users.noreply.github.com>
2019-03-03 17:23:38 -0800

    Fixed error in models.py

--- software/secbin/pastebin/models.py
+++ software/secbin/pastebin/models.py
@@ -23,11 +23,11 @@ class Profile(models.Model):
     
     def save(self): #overriding main save function
         super().save()
-        img = Image.open(self.image.path)
+        img = Image.open(self.profilePic.path)
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+            img.save(self.profilePic.path)
 
 
